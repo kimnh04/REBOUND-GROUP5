e9c5176af6287b93fdb2e2a3d9d9f693c7382d47
hoaloken61998 <138305808+hoaloken61998@users.noreply.github.com>
2025-03-17 21:29:32 +0700

    Delete src directory

--- src/app/models/customer.model.ts
+++
@@ -1,8 +0,0 @@
-export interface CustomerInterface {
-  Customer_ID: string;
-  Customer_Name: string;
-  Customer_Email: string;
-  Customer_Phone: string;
-  totalOrders?: number; // Dữ liệu có thể có hoặc không
-  selected?: boolean; // Biến boolean để chọn khách hàng
-}
